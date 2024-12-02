65c37afa841b182ed978eac247ffaa87822dd37c
ryo-is <ryosuke.izumi62@gmail.com>
2024-12-02 11:16:43 +0900

    feat: add @vanilla-extract/css

---
+++ client/src/index.css.ts
@@ -0,0 +1,23 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle(":root", {
+	fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+	lineHeight: 1.5,
+	fontWeight: 400,
+	colorScheme: "light dark",
+	color: "rgba(255, 255, 255, 0.87)",
+	backgroundColor: "#242424",
+	fontSynthesis: "none",
+	textRendering: "optimizeLegibility",
+	WebkitFontSmoothing: "antialiased",
+	MozOsxFontSmoothing: "grayscale",
+});
+
+globalStyle("body", {
+	margin: 0,
+	display: "flex",
+	justifyContent: "center",
+	placeItems: "center",
+	minWidth: "100vw",
+	minHeight: "100vh",
+});
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css.ts";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
